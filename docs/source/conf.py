008eb387ad5951555cd737f0b29bc1c54a0fcf9c
Christopher Sumnicht <csumnicht@berkeley.edu>
2026-08-18 20:01:30 -0700

    Version bump.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -9,7 +9,7 @@
 project = 'punyecs'
 copyright = '2026, Christopher Sumnicht'
 author = 'Christopher Sumnicht'
-release = '1.0.0'
+release = '1.1.0'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
